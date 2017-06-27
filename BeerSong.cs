0369195e52df8e3fe37d00f74c1ff29580b10d15
Cedd Burge <cedd.burge@res-ltd.com>
2017-06-27 08:37:24 +0100

    Add code for the 0th case

---
+++ BeerSong.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public static class BeerSong
+{
+    public static string Verse(int number)
+    {
+        if (number == 0)
+            return VerseZero();
+
+        throw new NotImplementedException("You need to implement this function.");
+    }
+
+    private static string VerseZero() =>
+        "No more bottles of beer on the wall, no more bottles of beer.\n" +
+        "Go to the store and buy some more, 99 bottles of beer on the wall.\n";
+
+
+    public static string Verses(int begin, int end)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+}
